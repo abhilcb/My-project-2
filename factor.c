64848d5f74fcdfe13ba1fd352e53b36bc2ce5254
abhilcb <157404116+abhilcb@users.noreply.github.com>
2024-01-23 17:08:36 +0530

    Add files via upload

---
+++ factor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void printFactors(int number) {
+    printf("Factors of %d are: ", number);
+
+    for (int i = 1; i <= number; ++i) {
+        if (number % i == 0) {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+}
+
+int main() {
+    int num;
+
+    // Get input from the user
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    // Check if the number is positive
+    if (num <= 0) {
+        printf("Please enter a positive integer.\n");
+        return 1; // Return error code
+    }
+
+    // Call the function to print factors
+    printFactors(num);
+
+    return 0; // Return success code
+}
---
+++ passedfun.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// Function to swap the values at the addresses passed
+void swapValues(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int num1, num2;
+
+    // Get input from the user
+    printf("Enter the value of variable 1: ");
+    scanf("%d", &num1);
+
+    printf("Enter the value of variable 2: ");
+    scanf("%d", &num2);
+
+    printf("Before swapping: Variable 1 = %d, Variable 2 = %d\n", num1, num2);
+
+    // Call the function to swap values
+    swapValues(&num1, &num2);
+
+    printf("After swapping: Variable 1 = %d, Variable 2 = %d\n", num1, num2);
+
+    return 0;
+}
